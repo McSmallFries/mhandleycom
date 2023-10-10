b5297c214326f70ce2232b6f7abbb789d27f09fa
Max Handley <maxh@tkc32m.com>
2023-10-10 01:08:29 +0100

    some more shiz

--- client/www/src/app/app.component.ts
+++ client/www/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { CookieOptions, CookieService } from 'ngx-cookie';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   theme = '';
